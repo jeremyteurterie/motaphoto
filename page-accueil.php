b7e5c24d6aaac129ae265871601bbb53773d8f16
Jeremy <jeremy.teurterie@gmail.com>
2024-02-02 16:26:48 +0700

    :sparkles: Responsive

--- page-accueil.php
+++ page-accueil.php
@@ -8,37 +8,44 @@ get_header();
 
 <main id="primary" class="site-main">
 
-    <div class="hero">
+    <!-- Chargement du hero -->
+
+    <div class="hero-area">
         <h1 class="title-hero">Photographe Event</h1>
         <img src="http://motaphoto.local/wp-content/uploads/2024/01/nathalie-11-scaled.jpeg" alt="image header">
     </div>
 
     <div class="post-photo">
         <form class='filter-form' method="get">
-            <div class="filter-1">
-                <select class="filter-categorie" name="categorie">
-                    <option value="">Catégorie</option>
-                    <?php
-                    // Récupérez les catégories WordPress et ajoutez-les aux options du sélecteur.
-                    $categories = get_terms('categorie', array('hide_empty' => false));
-                    foreach ($categories as $category) :
-                    ?>
-                        <option value="<?php echo esc_attr($category->term_id); ?>"><?php echo esc_html($category->name); ?></option>
-                    <?php endforeach; ?>
-                </select>
+            <div class="filter-container">
+                <div class="filter-1">
+                    <select class="filter-categorie" name="categorie">
+                        <option value="">Catégorie</option>
+                        <?php
+                        // Récupérez les catégories WordPress et ajoutez-les aux options du sélecteur.
+                        $categories = get_terms('categorie', array('hide_empty' => false));
+                        foreach ($categories as $category) :
+                        ?>
+                            <option value="<?php echo esc_attr($category->term_id); ?>"><?php echo esc_html($category->name); ?></option>
+                        <?php endforeach; ?>
+                    </select>
+                </div>
 
-                <select class="filter-format" name="format">
-                    <option value="">Format</option>
-                    <?php
-                    // Récupérez les termes de taxonomie "format" et ajoutez-les aux options du sélecteur.
-                    $formats = get_terms('format', array('hide_empty' => false));
-                    foreach ($formats as $format) :
-                    ?>
-                        <option value="<?php echo esc_attr($format->term_id); ?>"><?php echo esc_html($format->name); ?></option>
-                    <?php endforeach; ?>
-                </select>
+                <div class="filter-2">
+                    <select class="filter-format" name="format">
+                        <option value="">Format</option>
+                        <?php
+                        // Récupérez les termes de taxonomie "format" et ajoutez-les aux options du sélecteur.
+                        $formats = get_terms('format', array('hide_empty' => false));
+                        foreach ($formats as $format) :
+                        ?>
+                            <option value="<?php echo esc_attr($format->term_id); ?>"><?php echo esc_html($format->name); ?></option>
+                        <?php endforeach; ?>
+                    </select>
+                </div>
             </div>
-            <div class="filter-2">
+
+            <div class="filter-3">
                 <select class="filter-order" name="order">
                     <option value="">Trier par</option>
                     <option value="desc" <?php if ($order === "desc") : ?>selected<?php endif; ?>>A partir des plus récentes</option>
@@ -103,6 +110,7 @@ get_header();
 </main><!-- #main -->
 
 <?php
+the_content();
 get_sidebar();
 get_footer();
 ?>
