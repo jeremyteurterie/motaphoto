fd8ae2e8a775ab8d8e939be99991cde261273c77
Jeremy <jeremy.teurterie@gmail.com>
2024-02-07 10:41:08 +0700

    :sparkles: Responsive mobile

--- footer.php
+++ footer.php
@@ -15,15 +15,18 @@
 <footer id="colophon" class="site-footer">
 	<div class="site-info">
 		<ul class="footer">
-			<li class="item-footer">
-				<a href="http://motaphoto.local/mentions-legales/">Mentions légales</a>
-			</li>
-			<li class="item-footer">
-				<a href="http://motaphoto.local/vie-privee/">Vie privée</a>
-			</li>
-			<li class="item-footer">
-				<span>Tous droits réservés</span>
-			</li>
+			<div class="footer-container">
+				<li class="item-footer">
+					<a href="http://motaphoto.local/mentions-legales/">Mentions légales</a>
+				</li>
+				<li class="item-footer">
+					<a href="http://motaphoto.local/vie-privee/">Vie privée</a>
+				</li>
+				<li class="item-footer">
+					<span>Tous droits réservés</span>
+				</li>
+			</div>
+
 		</ul>
 	</div>
 </footer><!-- #colophon -->
