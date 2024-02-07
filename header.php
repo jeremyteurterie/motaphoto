efb7ac4e9f1ca206d0b0d238a1632df521ef2fe3
Jeremy <jeremy.teurterie@gmail.com>
2024-02-07 17:45:01 +0700

    :sparkles: Responsive done

--- header.php
+++ header.php
@@ -17,6 +17,7 @@
 <head>
 	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Nathalie Mota - Site personnel pour la vente de mes photos en impression HD.">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
 	<?php wp_head(); ?>
